d57cbe7dd16c79a05f292d4e6bdae37454c375e5
JSand89 <javier.esand@gmail.com>
2021-09-17 12:27:16 -0500

    faltan imagenes

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import {React, useState, useEffect} from 'react'
 import Data from '../../Data.json'
 import Card from '../Card/Card'
 import style from './Cards.module.css'
 
 function Cards() {
+    const [search,searchFunction]=useState('Search')
+    function cond(e){
+        e.Name.includes(search)
+        console.log(e.Name.includes(search))
+    }
+
+    var Filter=search=="Search"| search==''? Data: Data.filter(item=>item.Name.includes(search))
+    console.log(Filter)
+    useEffect(()=>{
+        Filter=search=="Search"| search==''? Data:Data.filter(cond)
+        console.log(Filter)
+
+
+    },[search])
     return (
+        <>
+        <input type="search" placeholder={search} 
+        onChange= {Event=>searchFunction(Event.target.value)} ></input>
         <div className={style.Cards}>
-            {Data.map(item=><Card props={item} key={item.id}/>)}
+            {Filter.map(item=><Card props={item} key={item.id}/>)}
         </div>
+        </>
     )
 }
 
